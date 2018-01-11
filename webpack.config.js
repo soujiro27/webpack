09b46bde4a930a6e29b04d583f21b82118b880c1
Joaquin Araujo <joaquinaraujojs@gmail.com>
2018-01-11 01:09:21 -0400

    Loaders y Plugins en Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname, 'index.js'),
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    // Here will be the loaders
+    rules: [
+      {
+        test: /\.css$/, // File Type
+        // use: ['style-loader', 'css-loader'] // Loader(s)
+        use: ExtractTextPlugin.extract({ // Loader + Extractor Plugin
+          use: 'css-loader'
+        })
+      }
+    ]
+  },
+  plugins: [
+    // Here will be the imported plugins
+    new ExtractTextPlugin('bundle.css')
+  ]
 }
